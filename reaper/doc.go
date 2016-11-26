3783b5aa24247bdd421dcb37d579f3df79b09d12
liut <liutao@licaigc.com>
2016-11-26 15:22:30 +0800

    定时任务执行器，主要用于清理类操作

---
+++ reaper/doc.go
@@ -0,0 +1,6 @@
+package reaper
+
+// Example:
+// defer reaper.Quit(reaper.Run(0, func() error {
+// 		// Cleanup
+// }))
---
+++ reaper/reaper.go
@@ -0,0 +1,50 @@
+package reaper
+
+import (
+	"log"
+	"time"
+)
+
+var defaultInterval = time.Minute * 5
+
+type CleanFunc func() error
+
+// Run invokes a reap function as a goroutine.
+func Run(interval time.Duration, cf CleanFunc) (chan<- struct{}, <-chan struct{}) {
+	if interval <= 0 {
+		interval = defaultInterval
+	}
+
+	quit, done := make(chan struct{}), make(chan struct{})
+	go reap(interval, cf, quit, done)
+	return quit, done
+}
+
+// Quit terminates the reap goroutine.
+func Quit(quit chan<- struct{}, done <-chan struct{}) {
+	quit <- struct{}{}
+	<-done
+}
+
+// reap with special action at set intervals.
+func reap(interval time.Duration, cf CleanFunc, quit <-chan struct{}, done chan<- struct{}) {
+	ticker := time.NewTicker(interval)
+
+	defer func() {
+		ticker.Stop()
+	}()
+
+	for {
+		select {
+		case <-quit:
+			// Handle the quit signal.
+			done <- struct{}{}
+			return
+		case <-ticker.C:
+			// Execute function of clean.
+			if err := cf(); err != nil {
+				log.Printf("reaper: ERROR: %v", err)
+			}
+		}
+	}
+}
