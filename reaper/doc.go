3ba2dd2e14388cc293c4d64c4ed6cc5e170a3b91
liut <liut@phoenixtv.com>
2020-06-06 21:51:42 +0800

    clean

--- reaper/doc.go
+++ reaper/doc.go
@@ -1,10 +1,10 @@
 //
-// 定时任务，用于资源清理
+// Package reaper 定时任务，用于资源清理
 //
 package reaper
 
 //
-// Example:
+// Example: default interval is 5 minutes
 // defer reaper.Quit(reaper.Run(0, func() error {
 // 		// Cleanup
 // }))
